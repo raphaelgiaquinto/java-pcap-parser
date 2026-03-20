04f97f78486fc005f6bba5ea409b5c715a133097
raphaelgiaquinto <raphaelgiaquinto@gmail.com>
2026-03-20 11:37:02 +0100

    feat(): parsing pcap header
    - Parsing of the pcap header section
    - refactor code to use java record instead of int[]

--- PcapParser.java
+++ PcapParser.java
@@ -1,3 +1,10 @@
+/**
+ * PcapParser written in java
+ * Based on the documentation found: https://datatracker.ietf.org/doc/draft-ietf-opsawg-pcap/
+ *
+ * @author raphaelgiaquinto
+ */
+
 import java.io.*;
 import java.util.*;
 import java.nio.*;
@@ -8,6 +15,10 @@ enum PcapFileFormat {
     BIG_ENDIAN_NANO_SECONDS,
     LITTLE_ENDIAN_NANO_SECONDS
 }
+
+record PcapVersion(int majorVersion, int minorVersion) {}
+
+record LinkTypeAndAdditionalInfo(int fcsLen, int r, int p, int reserved3, int linkType) {}
 
 void main(String[] args) {
 
@@ -36,9 +47,17 @@ void main(String[] args) {
         }
         IO.println("Pcap file format: " + pcapFileFormat);
         //the versions are on the next 4 bytes after the magic number
-        ByteBuffer versionsBuffer = ByteBuffer.wrap(pcapHeader, 4, 8);
-        int[] pcapVersions = getPcapVersions(pcapFileFormat, versionsBuffer);
-        IO.println("Pcap versions: " + Arrays.stream(pcapVersions).mapToObj(Integer::toString).collect(Collectors.joining(".")));
+        ByteBuffer versionsBuffer = ByteBuffer.wrap(pcapHeader, 4, 4);
+        PcapVersion pcapVersions = getPcapVersions(pcapFileFormat, versionsBuffer);
+        IO.println("Pcap versions: " + pcapVersions);
+        //the snap length is on the next 4 bytes after reserved1 and reserved2 sections (4 bytes each)
+        ByteBuffer snapLenBuffer = ByteBuffer.wrap(pcapHeader, 16, 4);
+        int snapLen = getSnapLen(pcapFileFormat, snapLenBuffer);
+        IO.println("Snap length: " + snapLen + " (max captured packet size in bytes)");
+        //the linktype is on the next 4 bytes after the snap length section
+        ByteBuffer linkTypeBuffer = ByteBuffer.wrap(pcapHeader, 20, 4);
+        LinkTypeAndAdditionalInfo linkTypeAndAdditionalInfo = getLinkTypeAndAdditionalInformation(pcapFileFormat, linkTypeBuffer);
+        IO.println("Link and additional information: " + linkTypeAndAdditionalInfo);
     } catch (IOException e) {
         IO.println("Error reading PCAP file: " + e.getMessage());
         System.exit(1);
@@ -78,11 +97,12 @@ PcapFileFormat getPcapFileFormat(ByteBuffer magicNumberBuffer) {
  * Extracts the major and minor versions from the pcap header
  * @param pcapFileFormat the pcap file format extracted from the magic number
  * @param versionsBuffer byte buffer containing the versions on 4 bytes
- * @return [majorVersion, minorVersion] as an int array of length 2
+ * @return the major and minor versions as record
  */
-int[] getPcapVersions(PcapFileFormat pcapFileFormat, ByteBuffer versionsBuffer) {
-    if (versionsBuffer.remaining() < 4) {
-        throw new RuntimeException("Versions buffer must contain at least 4 bytes");
+
+PcapVersion getPcapVersions(PcapFileFormat pcapFileFormat, ByteBuffer versionsBuffer) {
+    if (versionsBuffer.remaining() != 4) {
+        throw new RuntimeException("Versions buffer must contain 4 bytes");
     }
     switch (pcapFileFormat) {
         case BIG_ENDIAN_MICRO_SECONDS, BIG_ENDIAN_NANO_SECONDS:
@@ -93,6 +113,56 @@ int[] getPcapVersions(PcapFileFormat pcapFileFormat, ByteBuffer versionsBuffer) 
             break;
     }
     int majorVersion = versionsBuffer.getShort(); //a short is 2 bytes
-    int minorVersion = versionsBuffer.getShort(); //a short is 2 bytes
-    return new int[] { majorVersion, minorVersion };
+    int minorVersion = versionsBuffer.getShort(); //grab the next 2 bytes
+    return new PcapVersion(majorVersion, minorVersion);
+}
+
+/**
+ * Returns the snap length from the pcap header
+ * the snap length is the maximum number of bytes captured in each packet
+ * @param pcapFileFormat
+ * @param snapLenBuffer
+ * @return the snap length
+ */
+int getSnapLen(PcapFileFormat pcapFileFormat, ByteBuffer snapLenBuffer) {
+    if (snapLenBuffer.remaining() != 4) {
+        throw new RuntimeException("Snap len buffer must contain 4 bytes");
+    }
+    switch (pcapFileFormat) {
+        case BIG_ENDIAN_MICRO_SECONDS, BIG_ENDIAN_NANO_SECONDS:
+            snapLenBuffer.order(ByteOrder.BIG_ENDIAN);
+            break;
+        case LITTLE_ENDIAN_MICRO_SECONDS, LITTLE_ENDIAN_NANO_SECONDS:
+            snapLenBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            break;
+    }
+    return snapLenBuffer.getInt();
+}
+
+/**
+ * Returns the linktype and additional information from the pcap header
+ * the linktype is the type of the network layer protocol
+ * @param pcapFileFormat
+ * @param linkTypeAdditionalInfoBuffer
+ * @return the linktype and additional information as record
+ */
+LinkTypeAndAdditionalInfo getLinkTypeAndAdditionalInformation(PcapFileFormat pcapFileFormat, ByteBuffer linkTypeAdditionalInfoBuffer) {
+    if (linkTypeAdditionalInfoBuffer.remaining() != 4) {
+        throw new RuntimeException("Link type buffer must contain 4 bytes");
+    }
+    switch (pcapFileFormat) {
+        case BIG_ENDIAN_MICRO_SECONDS, BIG_ENDIAN_NANO_SECONDS:
+            linkTypeAdditionalInfoBuffer.order(ByteOrder.BIG_ENDIAN);
+            break;
+        case LITTLE_ENDIAN_MICRO_SECONDS, LITTLE_ENDIAN_NANO_SECONDS:
+            linkTypeAdditionalInfoBuffer.order(ByteOrder.LITTLE_ENDIAN);
+            break;
+    }
+    int block32bits = linkTypeAdditionalInfoBuffer.getInt();
+    int fcsLen = (block32bits >>> 28) & 0x0F;
+    int r = (block32bits >>> 27) & 0x01;
+    int p = (block32bits >>> 26) & 0x01;
+    int reserved3 = (block32bits >>> 16) & 0x03FF;
+    int linkType = (block32bits & 0xFFFF);
+    return new LinkTypeAndAdditionalInfo(fcsLen, r, p, reserved3, linkType);
 }
